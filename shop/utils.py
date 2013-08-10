8f8270e9e9a1c3add4895b5581c0dad0813d25b2
chenchiyuan <chenchiyuan03@gmail.com>
2013-08-10 20:52:24 +0800

    fix bug

--- shop/utils.py
+++ shop/utils.py
@@ -11,6 +11,8 @@ def datetime_format(datetime):
 def markdown(content, headers=""):
     if not headers:
         headers = [
+            """<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">""",
+            """<meta http-equiv="content-type" content="text/html; charset=UTF-8" />""",
             """<link href="/static/css/shop.css" rel="stylesheet" media="screen">""",
             """<link href="/static/css/jquery.mobile-1.3.1.min.css" rel="stylesheet" media="screen">"""
         ]
